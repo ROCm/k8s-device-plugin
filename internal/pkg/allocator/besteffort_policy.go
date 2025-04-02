4e0e694ea6f66054db36cda86ba80410c6cec067
vm <sriramr2230@gmail.com>
2025-04-02 08:54:48 +0000

    Topology aware gpu allocation
    -> Allocator package is introduced to calculate the best possible
       subset of GPUs to allocate when resources are requested by device
       plugin
    
    -> Device Plugin takes care of passing on GPU’s nodeId as part of the
       device struct which is used to fetch link types between that GPU and
       all other GPUs.
    
    -> Device Plugin also passes on GPU’s numaNode which is used in the
       scoring logic
    
    -> Kubelet informs Allocator how many resources are being requested and
       what are the available devices of that resource type
    
    -> Allocator generates all unique combinations of subsets of size n
    
    -> For every unique pair(x,y) in each subset, increment
       score += score(x,y)
    
    -> Pick the subset with the least score as the preferred allocation
    
    Logic for scoring each pair of devices
    
    -> If x.devId == y.devId are equal, score += 10, else, score += 20
    
     Explanation: If devId's are same, they are partitions of the same gpu
     which should be considered better than being part of different GPU’s
    
    -> If link_type(x,y) == "xgmi", score += 10
        If link_type(x,y) == "pcie", score += 40
        If link_type(x,y) is anything else, score += 50
    
    Explanation: XGMI connection is faster than PCIE
    
    Allocator fetches link_type for all links for a node `x` from the value
    present in:
    
    /sys/class/kfd/kfd/topology/nodes/x/io_links/<int>/properties
    /sys/class/kfd/kfd/topology/nodes/x/p2p_links/<int>/properties
    
    -> If x.numaNode == y.numaNode, score += 10, else, score +=20
    
    Explanation: GPUs being connected to the same numa node is better than
                 being connected to different numa nodes
    
    The value for the scores are picked in a way that the below order of
    precedence is followed for the preferred allocation, hence some values
    are higher than others as those properties of a pair have a bigger
    effect on how "good" the pair of gpu's is.
    
    1.	XGMI + same gpu + same numa node
    2.	XGMI + different gpu + same numa node
    3.	XGMI + different gpu + different numa node
    4.	PCIE + same gpu + same numa node
    5.	PCIE + different gpu + same numa node
    6.	PCIE + different gpu + different numa node
    7.	Any different link type other than XGMI and PCIE
    
    To avoid calculating a large number of subsets and their score,
    partitioned GPUs use their original GPU's properties and we avoid
    making extra subsets between partitions of same gpu since all
    partitions of a GPU will have same score with all partitions of another
    GPU.

---
+++ internal/pkg/allocator/besteffort_policy.go
@@ -0,0 +1,148 @@
+/**
+# Copyright 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the \"License\");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an \"AS IS\" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+**/
+
+package allocator
+
+import (
+	"fmt"
+	"math"
+
+	"github.com/golang/glog"
+)
+
+/**
+*  Best effort policy tries to come up with a subset of allocatable GPUs with best possible weight(connectivity).
+*  We calculate weight of every GPU pair. The weight takes into account below information:
+*  1. Type of link between the GPUs(XGMI or PCIE)
+*  2. For partitioned GPUs, it tries to assign weights based on whether partitions are of same GPU or different GPUs
+*  3. If both GPUs are part of same numa node or not
+*  Pair with lower weight takes higher precedence. We calculate the sum of weights b/n individual pair within a given
+*  subset and come up with total score for the subset. Subset with lowest score is given preference during allocation.
+**/
+
+const (
+	invalidSize         = "allocation size can not be negative"
+	invalidAvailable    = "available devices count less than allocation size"
+	invalidRequired     = "must_include devices size is more than allocation size"
+	invalidReqAvailable = "must_include length should be less than or equal to avilable device size"
+	invalidInit         = "Init method must be called before Allocate"
+	noCandidateFound    = "No candidate subset found with matching criteria"
+)
+
+type BestEffortPolicy struct {
+	devices          []*Device
+	devicesMap       map[string]*Device
+	devicePartitions map[string]*DevicePartitions
+	p2pWeights       map[int]map[int]int
+}
+
+func NewBestEffortPolicy() *BestEffortPolicy {
+	return &BestEffortPolicy{
+		devices:          make([]*Device, 0),
+		devicesMap:       make(map[string]*Device),
+		devicePartitions: make(map[string]*DevicePartitions),
+		p2pWeights:       make(map[int]map[int]int),
+	}
+}
+
+func (b *BestEffortPolicy) getDevicesFromIds(ids []string) []*Device {
+	var res []*Device
+	for _, id := range ids {
+		res = append(res, b.devicesMap[id])
+	}
+	return res
+}
+
+// Init initializes pair wise weights of all devices and stores in-memory
+func (b *BestEffortPolicy) Init(devs []*Device, topoDir string) error {
+	err := fetchAllPairWeights(devs, b.p2pWeights, topoDir)
+	if err == nil {
+		b.devices = devs
+		for idx := range devs {
+			b.devicesMap[devs[idx].Id] = devs[idx]
+		}
+		b.devicePartitions = groupPartitionsByDevId(devs)
+		for _, par := range b.devicePartitions {
+			glog.Infof("Device: %s Partitions: %v", par.ParentId, par.Devs)
+		}
+	}
+	return err
+}
+
+func (b *BestEffortPolicy) Allocate(availableIds, requiredIds []string, size int) ([]string, error) {
+	outset := []string{}
+	if size <= 0 {
+		return outset, fmt.Errorf(invalidSize)
+	}
+
+	if len(availableIds) < size {
+		return outset, fmt.Errorf(invalidAvailable)
+	}
+
+	if len(requiredIds) > size {
+		return outset, fmt.Errorf(invalidRequired)
+	}
+
+	if len(requiredIds) > len(availableIds) {
+		return outset, fmt.Errorf(invalidReqAvailable)
+	}
+
+	if len(b.devices) == 0 {
+		return outset, fmt.Errorf(invalidInit)
+	}
+
+	if len(availableIds) == size {
+		return availableIds, nil
+	}
+
+	if len(requiredIds) == size {
+		return requiredIds, nil
+	}
+
+	if len(b.p2pWeights) == 0 {
+		return outset, fmt.Errorf(invalidInit)
+	}
+
+	if !setContainsAll(availableIds, requiredIds) {
+		return outset, fmt.Errorf(noCandidateFound)
+	}
+
+	available := b.getDevicesFromIds(availableIds)
+	required := b.getDevicesFromIds(requiredIds)
+	allSubsets, err := getCandidateDeviceSubsets(b.devicePartitions, b.devices, available, required, size, b.p2pWeights)
+	if err != nil {
+		return outset, err
+	}
+
+	bestScore := math.MaxInt32
+	var candidate *DeviceSet
+	for _, subset := range allSubsets {
+		if subset.TotalWeight < bestScore {
+			candidate = subset
+			bestScore = subset.TotalWeight
+		}
+	}
+	for _, id := range candidate.Ids {
+		for _, d := range available {
+			if d.NodeId == id {
+				outset = append(outset, d.Id)
+				break
+			}
+		}
+	}
+	glog.Infof("best device subset:%v best score:%v", outset, candidate.TotalWeight)
+	return outset, nil
+}
