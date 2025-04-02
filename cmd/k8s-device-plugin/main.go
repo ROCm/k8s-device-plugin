a2d7cff2c117ffbc58f53ee9b383158a777b8aa8
vm <sriramr2230@gmail.com>
2025-04-02 09:13:24 +0000

    Device Plugin Resource Naming Strategy
    The resource_naming_strategy is a new flag which can be passed to the
    device plugin daemonset. The supported values for the flag are
    "single" and "mixed"
    
    Terms to understand before viewing the changes in this commit:
    
    Homogeneous Node:
       If all GPUs in a node are following the same compute and memory
       partition style, the node is considered homogeneous
    
    Heterogeneous Node:
       If the GPUs on a node have different different compute and memory
       partition styles, the node is considered heterogeneous (Put simply,
       if node is not homogeneous)
    
    Behaviour of Resource Naming Strategy in different node types:
    
    Homogeneous Node:
    
    -> If node is homogeneous and resource naming strategy is "single",
       one plugin is started using the DevicePluginManager with the last
       name as “gpu”.
    
       If node is homogeneous and resource naming strategy is "mixed",
       one plugin is started using the DevicePluginManager with the last
       name as the partition style present on the node.
    
    -> The ListAndWatch function remains almost the same as it was before.
       It reports resources under a single resource name(the name will
       either be "gpu" or the partition style present on the node(cpx_nps1)
       depending on strategy)
    
    Heterogeneous:
    
    -> If node is heterogeneous and resource naming strategy is "mixed", we
       invoke the DevicePluginManager to start multiple plugins for
       different partitionTypes under the names “spx-nps1, “cpx-nps1”, etc.
       We use the devicesCount map to start plugins for the partitionTypes
       that are present in the map
    
    -> ListAndWatch sends the devices to the plugin for their respective
       resource type depending on its partitionType. Each device has
       computePartition and memoryPartition fields in its object as shown
       before, which is used to identify which plugin to report the
       resource under. (amd.com/spx-nps1,amd.com/cpx-nps1, etc..)
    
    Note:
    -> If node is heterogeneous, "single" strategy is not supported as
       multiple resource types getting reported under a single resource
       name wouldn't be mathematically accurate as to how many true gpus of
       each type there are
    
    -> For nodes where partitioning is not supported(MI200), irrespective
       of strategy, the resources will get reported under "amd.com/gpu"
    
    -> If the flag is not set by user, default value is "single". This is
       to maintain backwards compatibility with older resource name before
       strategy was introduced (amd.com/gpu)

--- cmd/k8s-device-plugin/main.go
+++ cmd/k8s-device-plugin/main.go
@@ -32,25 +32,62 @@ import (
 
 var gitDescribe string
 
-func getResourceList() []string {
+type ResourceNamingStrategy string
+
+const (
+	StrategySingle ResourceNamingStrategy = "single"
+	StrategyMixed  ResourceNamingStrategy = "mixed"
+)
+
+func ParseStrategy(s string) (ResourceNamingStrategy, error) {
+	switch s {
+	case string(StrategySingle):
+		return StrategySingle, nil
+	case string(StrategyMixed):
+		return StrategyMixed, nil
+	default:
+		return "", fmt.Errorf("invalid resource naming strategy: %s", s)
+	}
+}
+
+func getResourceList(resourceNamingStrategy ResourceNamingStrategy) ([]string, error) {
 	var resources []string
 
 	// Check if the node is homogeneous
 	isHomogeneous := amdgpu.IsHomogeneous()
+	partitionCountMap := amdgpu.UniquePartitionConfigCount(amdgpu.GetAMDGPUs())
+	if len(amdgpu.GetAMDGPUs()) == 0 {
+		return resources, nil
+	}
 	if isHomogeneous {
-		// Homogeneous node will report only "gpu" resource
-		resources = []string{"gpu"}
+		// Homogeneous node will report only "gpu" resource if strategy is single. If strategy is mixed, it will report resources under the partition type name
+		if resourceNamingStrategy == StrategySingle {
+			resources = []string{"gpu"}
+		} else if resourceNamingStrategy == StrategyMixed {
+			if len(partitionCountMap) == 0 {
+				// If partitioning is not supported on the node, we should report resources under "gpu" regardless of the strategy
+				resources = []string{"gpu"}
+			} else {
+				for partitionType, count := range partitionCountMap {
+					if count > 0 {
+						resources = append(resources, partitionType)
+					}
+				}
+			}
+		}
 	} else {
-		// Heterogeneous node reports resources based on partition types
-		gpus := amdgpu.GetAMDGPUs()
-		partitionCountMap := amdgpu.UniquePartitionConfigCount(gpus)
-		for partitionType, count := range partitionCountMap {
-			if count > 0 {
-				resources = append(resources, partitionType)
+		// Heterogeneous node reports resources based on partition types if strategy is mixed. Heterogeneous is not allowed if Strategy is single
+		if resourceNamingStrategy == StrategySingle {
+			return resources, fmt.Errorf("Partitions of different styles across GPUs in a node is not supported with single strategy. Please start device plugin with mixed strategy")
+		} else if resourceNamingStrategy == StrategyMixed {
+			for partitionType, count := range partitionCountMap {
+				if count > 0 {
+					resources = append(resources, partitionType)
+				}
 			}
 		}
 	}
-	return resources
+	return resources, nil
 }
 
 func main() {
@@ -68,9 +105,16 @@ func main() {
 		flag.PrintDefaults()
 	}
 	var pulse int
+	var resourceNamingStrategy string
 	flag.IntVar(&pulse, "pulse", 0, "time between health check polling in seconds.  Set to 0 to disable.")
+	flag.StringVar(&resourceNamingStrategy, "resource_naming_strategy", "single", "Resource strategy to be used: single or mixed")
 	// this is also needed to enable glog usage in dpm
 	flag.Parse()
+	strategy, err := ParseStrategy(resourceNamingStrategy)
+	if err != nil {
+		glog.Errorf("%v", err)
+		os.Exit(1)
+	}
 
 	for _, v := range versions {
 		glog.Infof("%s", v)
@@ -96,8 +140,14 @@ func main() {
 		// /sys/class/kfd only exists if ROCm kernel/driver is installed
 		var path = "/sys/class/kfd"
 		if _, err := os.Stat(path); err == nil {
-			resources := getResourceList()
-			l.ResUpdateChan <- resources
+			resources, err := getResourceList(strategy)
+			if err != nil {
+				glog.Errorf("Error occured: %v", err)
+				os.Exit(1)
+			}
+			if len(resources) > 0 {
+				l.ResUpdateChan <- resources
+			}
 		}
 	}()
 	manager.Run()
