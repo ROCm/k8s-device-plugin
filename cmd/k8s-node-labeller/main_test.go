0c31bd2ea0e3bcf223afcf9a9d9b3fbae9bda599
yansun1996 <Yan.Sun3@amd.com>
2025-04-28 10:49:13 +0000

    test: Add unit tests for recent labeller changes
    Added more unit test cases specifically targeting the recent modifications in the node labeller to ensure their correctness and robustness.

--- cmd/k8s-node-labeller/main_test.go
+++ cmd/k8s-node-labeller/main_test.go
@@ -1,7 +1,119 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+var (
+	expectedAllLabelKeys = map[string]bool{
+		"amd.com/gpu.family":             true,
+		"amd.com/gpu.driver-version":     true,
+		"amd.com/gpu.driver-src-version": true,
+		"amd.com/gpu.firmware":           true,
+		"amd.com/gpu.device-id":          true,
+		"amd.com/gpu.product-name":       true,
+		"amd.com/gpu.vram":               true,
+		"amd.com/gpu.simd-count":         true,
+		"amd.com/gpu.cu-count":           true,
+	}
+	expectedAllExperimentalLabelKeys = map[string]bool{
+		"beta.amd.com/gpu.family":             true,
+		"beta.amd.com/gpu.driver-version":     true,
+		"beta.amd.com/gpu.driver-src-version": true,
+		"beta.amd.com/gpu.firmware":           true,
+		"beta.amd.com/gpu.device-id":          true,
+		"beta.amd.com/gpu.product-name":       true,
+		"beta.amd.com/gpu.vram":               true,
+		"beta.amd.com/gpu.simd-count":         true,
+		"beta.amd.com/gpu.cu-count":           true,
+	}
+)
+
+func TestInitLabelLists(t *testing.T) {
+	labelMap := map[string]bool{}
+	for _, label := range allLabelKeys {
+		labelMap[label] = true
+	}
+	if !reflect.DeepEqual(labelMap, expectedAllLabelKeys) {
+		t.Errorf("failed to get expected all labels during init, got %+v, expect %+v", labelMap, expectedAllLabelKeys)
+	}
+	experimentalLabelMap := map[string]bool{}
+	for _, label := range allExperimentalLabelKeys {
+		experimentalLabelMap[label] = true
+	}
+	if !reflect.DeepEqual(experimentalLabelMap, expectedAllExperimentalLabelKeys) {
+		t.Errorf("failed to get expected all experimental labels during init, got %+v, expect %+v", labelMap, expectedAllLabelKeys)
+	}
+}
+
+func TestRemoveOldNodeLabels(t *testing.T) {
+	testCases := []struct {
+		inputNode    *corev1.Node
+		expectLabels map[string]string
+	}{
+		{
+			inputNode: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"amd.com/gpu.cu-count":                          "104",
+						"amd.com/gpu.device-id":                         "740f",
+						"amd.com/gpu.driver-version":                    "6.10.5",
+						"amd.com/gpu.family":                            "AI",
+						"amd.com/gpu.product-name":                      "Instinct_MI210",
+						"amd.com/gpu.simd-count":                        "416",
+						"amd.com/gpu.vram":                              "64G",
+						"beta.amd.com/gpu.cu-count":                     "104",
+						"beta.amd.com/gpu.cu-count.104":                 "1",
+						"beta.amd.com/gpu.device-id":                    "740f",
+						"beta.amd.com/gpu.device-id.740f":               "1",
+						"beta.amd.com/gpu.family":                       "HPC",
+						"beta.amd.com/gpu.family.HPC":                   "1",
+						"beta.amd.com/gpu.product-name":                 "Instinct_MI300X",
+						"beta.amd.com/gpu.product-name.Instinct_MI300X": "1",
+						"beta.amd.com/gpu.simd-count":                   "416",
+						"beta.amd.com/gpu.simd-count.416":               "1",
+						"beta.amd.com/gpu.vram":                         "64G",
+						"beta.amd.com/gpu.vram.64G":                     "1",
+						"dummyLabel1":                                   "1",
+						"dummyLabel2":                                   "2",
+					},
+				},
+			},
+			expectLabels: map[string]string{
+				"dummyLabel1": "1",
+				"dummyLabel2": "2",
+			},
+		},
+		{
+			inputNode: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						"amd.com/cpu":    "true",
+						"amd.com/gpu":    "true",
+						"amd.com/mi300x": "true",
+						"dummyLabel1":    "1",
+						"dummyLabel2":    "2",
+					},
+				},
+			},
+			expectLabels: map[string]string{
+				"amd.com/cpu":    "true",
+				"amd.com/gpu":    "true",
+				"amd.com/mi300x": "true",
+				"dummyLabel1":    "1",
+				"dummyLabel2":    "2",
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		removeOldNodeLabels(tc.inputNode)
+		if !reflect.DeepEqual(tc.inputNode.Labels, tc.expectLabels) {
+			t.Errorf("failed to get expected node labels after removing old labels, got %+v, expect %+v", tc.inputNode.Labels, tc.expectLabels)
+		}
+	}
 }
