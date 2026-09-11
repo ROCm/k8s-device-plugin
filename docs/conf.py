c5a8d35af1205b0430b1ee87486aa76dcaa9ce54
Matt Elliott <matt.elliott@amd.com>
2026-09-11 10:52:50 -0400

    Switch theme flavor to instinct-design (#220)
    Aligns with the redesign rolled out in instinct-docs (MAT-49): https://github.com/ROCm/instinct-docs/pull/133

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved
 # Required settings
 html_theme = "rocm_docs_theme"
 html_theme_options = {
-    "flavor": "instinct",
+    "flavor": "instinct-design",
     "link_main_doc": True,
     "use_download_button": True,
 }
